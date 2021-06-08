5322e273b57b8aa46c3b836d76ed89913fad5e27
Pratap <pratap.1804@gmail.com>
2021-06-09 00:25:59 +0530

    Creating Contact Variables in Address Book

---
+++ AddressBookUC1.java
@@ -0,0 +1,22 @@
+public class AddressBookDetails {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Address Book");
+
+		new PersonalInformation();
+	}
+
+	public class PersonalInformation {
+
+		private String firstName;
+		private String lastName;
+		private int age;
+		private String address;
+		private String city;
+		private String state;
+		private int zip;
+		private int phoneNumber;
+		private String eMail;
+
+	}
+}
